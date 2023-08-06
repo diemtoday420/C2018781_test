c8d60473fb87f6a037d9039afdee65a973ef5555
diemtoday420 <141099874+diemtoday420@users.noreply.github.com>
2023-08-06 12:00:46 +0900

    Update newsKeywordBot.py

--- newsKeywordBot.py
+++ newsKeywordBot.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     bot = telegram.Bot(token=bot_token)
 
     # 가장 최근에 온 메세지의 정보 중, chat id만 가져옴 (이 chat id는 사용자(나)의 계정 id임)
-    chat_id = bot.getUpdates()[-1].message.chat.id
+    chat_id = '6250265022' #bot.getUpdates()[-1].message.chat.id
 
     # 검색할 키워드 설정
     queries = ["삼성카드", "신한카드", "현대카드", "국민카드", "신용카드", "금융감독원"]
@@ -67,7 +67,7 @@ if __name__ == '__main__':
     # 각 키워드에 대해 한 번씩 실행
     for query in queries:
         send_links(query)
-        time.sleep(3)
+        time.sleep(1)
     
     # 프로그램이 끝났음을 알리기 위한 메시지 전송
     bot.sendMessage(chat_id=chat_id, text='크롤링이 완료되었습니다.')
